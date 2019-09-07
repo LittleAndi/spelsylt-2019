c8a8b4832686931f3c968fc9070cd1c4892f0034
Lars Nyström <nystrom.lasse@gmail.com>
2019-09-07 15:16:23 +0200

    Set window size

--- main.lua
+++ main.lua
@@ -3,7 +3,8 @@ Game = require('src.Game')
 local game = {}
 
 function love.load()
-    width, height = love.graphics.getDimensions( )
+    love.window.setMode(1920, 800, {resizable=false, vsync=false})
+    width, height = love.graphics.getDimensions()
 
     game = Game.new()
 end
